42f3bda801e12f4208721bf2cd85de4a1b1f4a6b
Shambu TN <shambutn@gmail.com>
2025-08-08 12:02:58 +0530

    added gitignore

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ import pg from "pg";
 import multer from "multer";
 import env from "dotenv";
 
-
 const app = express();
 const port = 3000;
 env.config();
@@ -13,15 +12,14 @@ env.config();
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    return cb(null, './uploads')
+    return cb(null, "./uploads");
   },
   filename: function (req, file, cb) {
-    return cb(null,  `${Date.now()}-${file.originalname}`)
-  }
+    return cb(null, `${Date.now()}-${file.originalname}`);
+  },
 });
 
-const upload=multer({storage});
-
+const upload = multer({ storage });
 
 const db = new pg.Client({
   user: "postgres",
@@ -32,20 +30,17 @@ const db = new pg.Client({
 });
 db.connect();
 
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(express.static("uploads"));
 
-
 let currentUserId = 1;
- let users = [
+let users = [
   // { id: 1, name: "Angela", color: "teal" },
   // { id: 2, name: "Jack", color: "powderblue" },
- ];
- let images=[];
- let ratingsarray=[];
- 
+];
+let images = [];
+let ratingsarray = [];
 
 async function checkVisisted() {
   const result = await db.query(
@@ -58,104 +53,112 @@ async function checkVisisted() {
   });
   return countries;
 }
- async function getcurrentuser(){
-  const result=await db.query("SELECT * FROM users");
-  users=result.rows;
+async function getcurrentuser() {
+  const result = await db.query("SELECT * FROM users");
+  users = result.rows;
   console.log(users);
- return users.find((user)=> user.id == currentUserId);
- }
+  return users.find((user) => user.id == currentUserId);
+}
 
- async function getimages(user_id,country_code) {
-  const result=await db.query("SELECT * from image_table where user_id=$1 and country_code=$2",[user_id,country_code]);
-  images=result.rows;
+async function getimages(user_id, country_code) {
+  const result = await db.query(
+    "SELECT * from image_table where user_id=$1 and country_code=$2",
+    [user_id, country_code]
+  );
+  images = result.rows;
   return images;
- }
+}
 
- async function getratedcountries() {
-  const result=await db.query("SELECT * from countries where rating>4");
-  ratingsarray=result.rows;
+async function getratedcountries() {
+  const result = await db.query("SELECT * from countries where rating>4");
+  ratingsarray = result.rows;
   //console.log(ratingsarray);
   return ratingsarray;
-  
- }
+}
 
-
- app.get("/",async(req,res)=>{
+app.get("/", async (req, res) => {
   res.render("frontend.ejs");
- });
- 
- app.get("/images",async (req,res)=>{
-   const cratings=await getratedcountries();
-  res.render("images.ejs",{
-    users:images,
-   ratings:cratings,
+});
+
+app.get("/images", async (req, res) => {
+  const cratings = await getratedcountries();
+  res.render("images.ejs", {
+    users: images,
+    ratings: cratings,
   });
 });
 
-app.get("/login",async(req,res)=>{
+app.get("/login", async (req, res) => {
   res.render("login.ejs");
 });
 
- app.post("/upload",upload.single("Uploadimage"), async(req,res)=>{
+app.post("/upload", upload.single("Uploadimage"), async (req, res) => {
   console.log(req.file);
-  const id=req.body["country"];
-  const username=req.body["username"];
+  const id = req.body["country"];
+  const username = req.body["username"];
 
-  try{
-        const result=await db.query("SELECT country_code from countries where LOWER(country_name) LIKE $1 || '%' ;",[id.toLowerCase()]);
-        const result1=await db.query("SELECT id from users where LOWER(name) LIKE $1 || '%' ;",[username.toLowerCase()]);
-      try{
-        const data = result.rows[0];
-    const countryCode = data.country_code;
-    const data1 = result1.rows[0];
-    const user_id = data1.id;
-    console.log(countryCode);
+  try {
+    const result = await db.query(
+      "SELECT country_code from countries where LOWER(country_name) LIKE $1 || '%' ;",
+      [id.toLowerCase()]
+    );
+    const result1 = await db.query(
+      "SELECT id from users where LOWER(name) LIKE $1 || '%' ;",
+      [username.toLowerCase()]
+    );
+    try {
+      const data = result.rows[0];
+      const countryCode = data.country_code;
+      const data1 = result1.rows[0];
+      const user_id = data1.id;
+      console.log(countryCode);
       await db.query(
         "INSERT INTO image_table (country_code,image,user_id) VALUES ($1, $2,$3)",
-        [countryCode,req.file.path,user_id]
+        [countryCode, req.file.path, user_id]
       );
-    }catch{
+    } catch {
       console.log("Not entered in database");
     }
-  }
-  catch{
-          console.log("Country doesn't exist, enter correctly");
+  } catch {
+    console.log("Country doesn't exist, enter correctly");
   }
   res.redirect("/");
- });
+});
 
-
-
- app.post("/getimages",async(req,res)=>{
-  const inputuser=req.body["user"];
-  const inputcountry=req.body["country"];
-  try{
-    const result=await db.query("SELECT country_code from countries where LOWER(country_name) LIKE $1 || '%' ;",[inputcountry.toLowerCase()]);
-    const result1=await db.query("SELECT id from users where LOWER(name) LIKE $1 || '%' ;",[inputuser.toLowerCase()]);
-  try{
-    const data = result.rows[0];
-const countryCode = data.country_code;
-const data1 = result1.rows[0];
-const user_id = data1.id;
-   const usersimage=await getimages(user_id,countryCode);
-   console.log(usersimage);
-   res.render("images.ejs",{
-    users:usersimage,
-    ratings:ratingsarray,
-   });
- }catch{
-  console.log("Cannot get the images");
- }
-  }catch{
+app.post("/getimages", async (req, res) => {
+  const inputuser = req.body["user"];
+  const inputcountry = req.body["country"];
+  try {
+    const result = await db.query(
+      "SELECT country_code from countries where LOWER(country_name) LIKE $1 || '%' ;",
+      [inputcountry.toLowerCase()]
+    );
+    const result1 = await db.query(
+      "SELECT id from users where LOWER(name) LIKE $1 || '%' ;",
+      [inputuser.toLowerCase()]
+    );
+    try {
+      const data = result.rows[0];
+      const countryCode = data.country_code;
+      const data1 = result1.rows[0];
+      const user_id = data1.id;
+      const usersimage = await getimages(user_id, countryCode);
+      console.log(usersimage);
+      res.render("images.ejs", {
+        users: usersimage,
+        ratings: ratingsarray,
+      });
+    } catch {
+      console.log("Cannot get the images");
+    }
+  } catch {
     console.log("User has not visited the country");
   }
-
- });
-
+});
 
 app.get("/getstart", async (req, res) => {
   const countries = await checkVisisted();
-  const currentuser=await getcurrentuser();
+  const currentuser = await getcurrentuser();
   res.render("index.ejs", {
     countries: countries,
     total: countries.length,
@@ -165,7 +168,7 @@ app.get("/getstart", async (req, res) => {
 });
 app.post("/add", async (req, res) => {
   const input = req.body["country"];
- // const cuurentuser=await getcurrentuser();
+  // const cuurentuser=await getcurrentuser();
 
   try {
     const result = await db.query(
@@ -178,43 +181,42 @@ app.post("/add", async (req, res) => {
     try {
       await db.query(
         "INSERT INTO visited_countries (country_code,user_id) VALUES ($1, $2)",
-        [countryCode,currentUserId]
+        [countryCode, currentUserId]
       );
       res.redirect("/getstart");
     } catch (err) {
       console.log(err);
-      const countries=await checkVisisted();
-      const currentuser=await getcurrentuser();
-      res.render("index.ejs",{
-        countries:countries,
-        total:countries.length,
+      const countries = await checkVisisted();
+      const currentuser = await getcurrentuser();
+      res.render("index.ejs", {
+        countries: countries,
+        total: countries.length,
         users: users,
         color: currentuser.color,
-        error:"Country is already visited try again.",
+        error: "Country is already visited try again.",
       });
     }
   } catch (err) {
     console.log(err);
-    const countries=await checkVisisted();
-    const currentuser=await getcurrentuser();
-      res.render("index.ejs",{
-        countries:countries,
-        total:countries.length,
-        users: users,
-        color: currentuser.color,
-        error:" country does not exits try again.",
-      });
+    const countries = await checkVisisted();
+    const currentuser = await getcurrentuser();
+    res.render("index.ejs", {
+      countries: countries,
+      total: countries.length,
+      users: users,
+      color: currentuser.color,
+      error: " country does not exits try again.",
+    });
   }
 });
 app.post("/user", async (req, res) => {
-  if(req.body.add==="new")
-  {
+  if (req.body.add === "new") {
     res.render("new.ejs");
-  }else{
-  currentUserId=req.body.user;
-  console.log(currentUserId);
- 
-  res.redirect("/getstart");
+  } else {
+    currentUserId = req.body.user;
+    console.log(currentUserId);
+
+    res.redirect("/getstart");
   }
 });
 
@@ -222,16 +224,19 @@ app.post("/new", async (req, res) => {
   //Hint: The RETURNING keyword can return the data that was inserted.
   //https://www.postgresql.org/docs/current/dml-returning.html
 
-  const newusername=req.body.name;
-  const newusercolor=req.body.color;
-  const result=await db.query("INSERT INTO users (name,color) VALUES($1,$2) RETURNING *;",[newusername,newusercolor] )
-  const id=result.rows[0].id;
-  currentUserId=id;
+  const newusername = req.body.name;
+  const newusercolor = req.body.color;
+  const result = await db.query(
+    "INSERT INTO users (name,color) VALUES($1,$2) RETURNING *;",
+    [newusername, newusercolor]
+  );
+  const id = result.rows[0].id;
+  currentUserId = id;
   res.redirect("/getstart");
 });
 
-app.post("/delete",async (req,res)=>{
-  const input=req.body["country"];
+app.post("/delete", async (req, res) => {
+  const input = req.body["country"];
   console.log(input);
   try {
     const result = await db.query(
@@ -241,38 +246,38 @@ app.post("/delete",async (req,res)=>{
 
     const data = result.rows[0];
     const countryCode = data.country_code;
-  try{
-    await db.query("DELETE FROM visited_countries WHERE country_code=$1 AND user_id=$2",[countryCode,currentUserId]);
-    res.redirect("/getstart");
-  }catch(err){
-     console.log(err);
-     const countries=await checkVisisted();
-     const currentuser=await getcurrentuser();
-     res.render("index.ejs",{
-       countries:countries,
-       total:countries.length,
-       users: users,
-       color: currentuser.color,
-       error:"Country is not visited try again.",
-     });
-  }
-  }catch(err){
-    console.log(err);
-    console.log(err);
-    const countries=await checkVisisted();
-    const currentuser=await getcurrentuser();
-      res.render("index.ejs",{
-        countries:countries,
-        total:countries.length,
+    try {
+      await db.query(
+        "DELETE FROM visited_countries WHERE country_code=$1 AND user_id=$2",
+        [countryCode, currentUserId]
+      );
+      res.redirect("/getstart");
+    } catch (err) {
+      console.log(err);
+      const countries = await checkVisisted();
+      const currentuser = await getcurrentuser();
+      res.render("index.ejs", {
+        countries: countries,
+        total: countries.length,
         users: users,
         color: currentuser.color,
-        error:" country does not exits try again.",
+        error: "Country is not visited try again.",
       });
+    }
+  } catch (err) {
+    console.log(err);
+    console.log(err);
+    const countries = await checkVisisted();
+    const currentuser = await getcurrentuser();
+    res.render("index.ejs", {
+      countries: countries,
+      total: countries.length,
+      users: users,
+      color: currentuser.color,
+      error: " country does not exits try again.",
+    });
   }
 });
-
-
-
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
